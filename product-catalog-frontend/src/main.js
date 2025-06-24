32a7a862adcd84d0edfef2c9da13074b22e1466c
Isaac Bilyea <isaacbilyea@gmail.com>
2025-06-24 12:19:34 -0400

    Finish front end functionality
    Finish front end functionality with basic add delete and edit capabilities

--- product-catalog-frontend/src/main.js
+++ product-catalog-frontend/src/main.js
@@ -1,14 +1,42 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 import App from './App.vue'
 import router from './router'
+
+//PrimeVue Components
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Textarea from 'primevue/textarea'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import Dialog from 'primevue/dialog'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+
+//PrimeVue CSS
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+app.use(ToastService)
+
+//Register PrimeVue components globally
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.component('Textarea', Textarea)
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('Dialog', Dialog)
+app.component('Toast', Toast)
 
 app.mount('#app')
